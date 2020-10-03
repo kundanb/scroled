69af575b5824b341d9145d6e7d1d05a9a239c1ed
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2020-10-03 14:50:58 +0530

    updated main js

--- src/js/main.js
+++ src/js/main.js
@@ -22,11 +22,13 @@ HTMLElement.prototype.hide = function (callback = null) {
   }
 };
 
-// load font css
-const fontCSSFile = document.createElement('link');
-fontCSSFile.href =
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
-fontCSSFile.rel = 'stylesheet';
+(() => {
+  // load font css
+  const fontCSSFile = document.createElement('link');
+  fontCSSFile.href =
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
+  fontCSSFile.rel = 'stylesheet';
 
-// start load
-document.head.appendChild(fontCSSFile);
+  // start load
+  document.head.appendChild(fontCSSFile);
+})();
