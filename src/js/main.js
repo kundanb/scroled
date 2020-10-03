9ad8218e0e30aa6233d6959a1dad357238c17870
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2020-10-03 14:07:17 +0530

    added font css

--- src/js/main.js
+++ src/js/main.js
@@ -21,3 +21,12 @@ HTMLElement.prototype.hide = function (callback = null) {
     setTimeout(callback, transDur);
   }
 };
+
+// load font css
+const fontCSSFile = document.createElement('link');
+fontCSSFile.href =
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
+fontCSSFile.rel = 'stylesheet';
+
+// start load
+document.head.appendChild(fontCSSFile);
